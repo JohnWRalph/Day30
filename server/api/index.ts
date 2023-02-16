b082073e9fd6f0da990810c1d330c337110bab41
JohnWRalph <John.Ralph.Git@gmail.com>
2023-02-15 18:42:12 -0600

    m

--- server/api/index.ts
+++ server/api/index.ts
@@ -8,8 +8,9 @@ import validateUserCreate from "../validateUserCreate";
 // import dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 // dotenv.config()
 const app = express()
-app.use(express.json({ limit: '50mb' }))
 app.use(cors())
+app.use(express.json({ limit: '50mb' }))
+
 
 let errorMessage;
 let alertMessage;
